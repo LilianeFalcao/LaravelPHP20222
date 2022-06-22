3b1eb24d9332859fec40706b457bfca75299add2
LilianeFalcao <lilianefalcao2293@gmail.com>
2022-06-22 10:34:15 -0300

    ate ent ta bom

---
+++ app/Http/Controllers/MensagemTopicoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MensagemTopico;
+use Illuminate\Http\Request;
+
+class MensagemTopicoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MensagemTopico  $mensagemTopico
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MensagemTopico $mensagemTopico)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MensagemTopico  $mensagemTopico
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MensagemTopico $mensagemTopico)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MensagemTopico  $mensagemTopico
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MensagemTopico $mensagemTopico)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MensagemTopico  $mensagemTopico
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MensagemTopico $mensagemTopico)
+    {
+        //
+    }
+}
