a17a2caac2e48184aae8fda9f185719bf28acea6
LilianeFalcao <lilianefalcao2293@gmail.com>
2022-07-08 14:26:04 -0300

    ddddddddddddddd

--- resources/views/restrict/layout.blade.php
+++ resources/views/restrict/layout.blade.php
@@ -12,7 +12,7 @@
 <body>
     <header>
         <picture>
-            <img src="{{ asset('img/logo.svg') }}" alt="Logo"/>
+            <img src="{{ asset('img/logo.png') }}" alt="Logo"/>
         </picture>
 
         <nav>
