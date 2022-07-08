6e8a3c7f8f79abee0252326f7d3f62822e58b91a
LilianeFalcao <lilianefalcao2293@gmail.com>
2022-07-08 14:28:48 -0300

    ddddddddddddddd

--- resources/views/restrict/mensagem/create.blade.php
+++ resources/views/restrict/mensagem/create.blade.php
@@ -1,8 +1,7 @@
 @extends('restrict.layout')
 
 @section('content')
-@if(count ($errors) > 0)
-
+@if(count($errors) > 0)
 <ul class="validator">
     @foreach($errors->all() as $error)
     <li>{{$error}}</li>
@@ -14,24 +13,24 @@
     @method('POST')
     <div>
         <label for="titulo">Título</label>
-        <input type="text" name="titulo" id="titulo" value="{{ old('titulo') }}" required />
+        <input type="text" name="titulo" id = "titulo" value="{{old('titulo') }}" required/>
     </div>
     <div>
         <label for="msg">Mensagem</label>
-        <textarea name="mensagem" id="msg" required>{{ old('mensagem') }}</textarea>
+        <textarea name="mensagem" id="msg" required>{{old('mensagem')}} </textarea>
     </div>
     <div>
         <label>
-            Topicos
-            <a href="{{url('topico/create')}}" class="button"> Add Topico</a>
+            Tópicos
+            <a href="{{url('topico/create')}}" class="button">Novo Tópico</a>
         </label>
         <div class="sub">
-            @foreach($topicos as @topico)
-            <input type="checkbox" id="top{{$topico->id}}" value="{{$topico->id}}" name="topico[]" @if($topico->id==old('topico_id')) checked @endif /> 
-            <label for="top{{$topico->id}}">{{$topico->id}}</label>          
-            @endforeach              
+            @foreach($topicos as $topico)
+            <input type="checkbox" id="top{{$topico->id}}" value="{{$topico->id}}" name="topico[]" @if($topico->id==old('topico_id')) checked @endif />
+            <label for="top{{$topico->id}}">{{$topico->topico}}</label>
+            @endforeach
         </div>
     </div>
-    <button type="submit" class="button"> Salvar </button>
+    <button type="submit" class="button">Salvar</button>
 </form>
 @endsection
