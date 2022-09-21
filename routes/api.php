54104c5a80ab4013dc458f38d10bdfaf10cc20bd
LilianeFalcao <lilianefalcao2293@gmail.com>
2022-09-21 10:35:33 -0300

    pag 7 pdf

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\MensagemController;
 use App\Http\Controllers\API\TopicoController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::put('/user', [UserController::class, 'update']);
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::resource('topico', TopicoController::class);
     Route::resource('mensagem', MensagemController::class);
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,21 @@ module.exports = {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors:{
+            black: '#000000',
+            white: '#ffffff',
+            primary: '#36393f',
+            primaryLight: '#CFD8DC',
+            purple: '#1d1f22',
+            orange: '#FFB74D',
+            orangeLight: '#FFE9CA',
+            fundo: "#383646",
+            perfil:"#B5B2BF",
+            pop: "#3C3945",
+            postbotao: "#3C3945",
+            Card: "#777382",
+            qrCode: "#383646"
+        },
     },
 
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
